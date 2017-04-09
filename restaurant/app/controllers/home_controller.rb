b190526d321468ff216ae4e1765c569fd1ef1ef9
Eryn Sawyer <elsawyer@email.wm.edu>
2017-04-08 22:03:06 -0400

    idk it works better now

--- restaurant/app/controllers/home_controller.rb
+++ restaurant/app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-  	query = "SELECT * FROM restaurant_vital LIMIT 10"
-  	@suggestions = RestaurantVital.connection.execute(query)
+  	prefs = ["%italian%", "%sushi%"]
+  	#order by random helps keep us from offering same suggestion every time
+  	#restaurants = self.connection_select_value("SELECT * FROM restaurant_vital ORDER BY RANDOM() LIMIT 4 ")
+  	#query = "SELECT * FROM restaurant_vital LIMIT 10"
+  	#@suggestions = RestaurantVital.connection.execute(query)
+    #@suggestions = RestaurantVital.limit(4)
+    @suggestions = RestaurantVital.where("restaurant_type ilike any ( array[?] )", prefs).limit(4).order("RANDOM()")
   end
 end
--- restaurant/app/models/restaurant_vital.rb
+++ restaurant/app/models/restaurant_vital.rb
@@ -1,10 +1,4 @@
 class RestaurantVital < ActiveRecord::Base
   self.table_name = 'restaurant_vital'
   self.primary_key = 'id'
-  class << self
-  	def self.findNearby()
-  		restaurants = self.connection_select_value("SELECT * FROM restaurant_vital LIMIT 10")
-  		return restaurants 
-  	end
-  end
 end
