7e1f406ed0613fddca65c8c82608c8b2c7fad6c3
Eryn Sawyer <elsawyer@email.wm.edu>
2017-04-13 09:20:43 -0400

    add broken bootstrap

--- restaurant/config/initializers/assets.rb
+++ restaurant/config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( form-elements.css )
+Rails.application.config.assets.precompile += %w( mdb.css )
+
+Rails.application.config.assets.precompile += %w( register.css )
+Rails.application.config.assets.precompile += %w( scripts.js  )
+Rails.application.config.assets.precompile += %w( mdb.js )
