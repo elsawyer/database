dde4ec807bb609304f42bdd04a9cf403fe70098f
Eryn Sawyer <elsawyer@email.wm.edu>
2017-04-18 17:51:59 -0400

    uses user location to get user's current time and day of the week

--- restaurant/config/application.rb
+++ restaurant/config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Timezone::Lookup.config(:geonames) do |c|
+   	c.username = 'elsawyer'
+ end
+
 module Restaurant
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
