defdf240d8cc1848586161f1ebf90d0bee91ef60
Eryn Sawyer <elsawyer@email.wm.edu>
2017-04-21 21:45:15 -0400

    merge in frontend

--- restaurant/app/controllers/home_controller.rb
+++ restaurant/app/controllers/home_controller.rb
@@ -1,32 +1,31 @@
 class HomeController < ApplicationController
-	def index
-		#sleep until HTML 5 gets permission for location tracking
-		#TODO: if no permission, maybe link to an error page - rn probably sleeps forever
-		while (!cookies[:lat_lng]) 
-			sleep(1)
-		end
 
-		#hardcoded user id - replace with login info later on
+	def index
+		#TODO: if no permission, maybe link to an error page - rn probably sleeps forever
+
 		@user = 'test'
 
 		#turn cookies into array
-		@lat_lng = cookies[:lat_lng].split("|")
-		lat = (@lat_lng[0]).to_f
-		long = (@lat_lng[1]).to_f
+		#@lat_lng = cookies[:lat_lng].split("|")
+		#lat = (@lat_lng[0]).to_f
+		#long = (@lat_lng[1]).to_f
+		lat = request.location.latitude
+		long = request.location.longitude 
 
-		#hardcoded prefs for now
+		#hardcode if on localhost
+		if request.ip == '::1'
+			lat = 37.272260
+			long = -76.707002
+		end
+
 		prefs = []
-		prefsStr = ["%italian%", "%sushi%"]
-
-
-		addr = Geocoder.address([lat, long]).split(",")
-		city = addr[1].strip()
-		state  = addr[2].split(" ")[0].strip()
-		cityState = "%" + city + " " + state + "%"
+		prefsStr = []
+		recd= []
 
 		timezone = Timezone.lookup(lat,long)
 		curTime = Time.now.in_time_zone(timezone.name).strftime("%H:%M")
 		weekDay = Time.now.in_time_zone(timezone.name).strftime("%A")
+		weekDay = "%"+weekDay
 		print curTime + "\n\n"
 		print weekDay + "\n\n"
 
@@ -37,17 +36,10 @@ class HomeController < ApplicationController
 		drinks = userData["drinks"]
 		homelat = userData["homelat"]
 		homelong = userData["homelong"]
-
-		#calculate user's home address
-		homeAddr = Geocoder.address([homelat, homelong]).split(",")
-		homeCity = homeAddr[1].strip()
-		homeState  = homeAddr[2].split(" ")[0].strip()
-		homeCityState = "%" + homeCity + " " + homeState + "%"
-		print homeCityState + "\n\n"
-
+		distance = 0
 
 		#query the userlikes db to get user selected preferences
-		userPrefs = Userlike.select("foodlike").where("userid = ?", @user)
+		userPrefs = Userlike.select("foodlike").where("userid = ? AND isLike=TRUE", @user)
 
 		#iterate through prefs, add to list of normal strings, add to list of "regex" strings
 		userPrefs.each do |post|
@@ -58,22 +50,59 @@ class HomeController < ApplicationController
 			prefs.push(post)
 		end
 
-		#query suggestions database and add to the pref list
+		userDislikes = Userlike.select("foodlike").where("userid = ? AND isLike=FALSE", @user)
+
+		dislikes=  []
+
+		userDislikes.each do |dislike|
+			dislikes.push(dislike["foodlike"])
+		end
+
+		#query suggestions database and add to the pref list: should not already be in pref list or in dislikes list
 		recs = GenericType.select("related").where("typename = ANY ( array[?] )", prefs)
 		recs.each do |rec|
 			rec = rec["related"]
-			add_rec = "%" + rec + "%"
+			if (prefs.include? rec) == false and (dislikes.include? rec) == false
+				add_rec = "%" + rec + "%"
+			end
 			prefsStr.push(add_rec)
+			recd.push(rec)
 		end
 
+		formatStr = "HH24:MI"
+
+		times = Restauranttime.where("day = weekday AND to_timestamp(?) < close AND to_timestamp(?) > open", weekDay, curTime)
 
   		#order by random helps keep us from offering same suggestion every time
   		#distance function is loaded into db - uses the Haversine formula to calculate linear distance between longs and lats
-    	@suggestions = RestaurantVital.where("restaurant_type ilike any ( array[?] ) AND location like ?", prefsStr, cityState).where(" ( distance ( latitude::REAL, longitude::REAL, ?::REAL, ?::REAL ) / 1609.344 ) <= 3", lat, long).limit(3).order("RANDOM()")
-    	print @suggestions[1]["phone"] + "\n\n"
+    	@suggestions = RestaurantVital.where("restaurant_vital.restaurant_type ilike any ( array[?] ) AND ( distance ( latitude::REAL, longitude::REAL, ?::REAL, ?::REAL ) / 1609.344 ) <= 50", prefsStr, lat, long).joins("JOIN restaurant_theme ON restaurant_vital.id = restaurant_theme.id JOIN restaurant_times ON restaurant_times.id = restaurant_vital.id").where("char_length(price) >= ? AND char_length(price) <= ? AND rating::REAL >= 3.0", minPrice, maxPrice).where("day like ? AND to_timestamp(?, ?)::time without time zone BETWEEN open AND close", weekDay, curTime, formatStr).limit(3).order("RANDOM()")
+
+    	#try to handle "dead zones" where user's preferences are too specific for the area
+    	if @suggestions.length < 2
+    		#night-owl mode: check the time and see if its too late for anything to be open
+
+    		#if user is willing to travel first, then increase distance by 10 miles
+    		if distance == 1
+    			@suggestions = RestaurantVital.where("restaurant_vital.restaurant_type ilike any ( array[?] ) AND ( distance ( latitude::REAL, longitude::REAL, ?::REAL, ?::REAL ) / 1609.344 ) <= 15", prefsStr, lat, long).joins("JOIN restaurant_theme ON restaurant_vital.id = restaurant_theme.id JOIN restaurant_times ON restaurant_times.id = restaurant_vital.id").where("char_length(price) >= ? AND char_length(price) <= ? AND rating::REAL >= 3.0", minPrice, maxPrice).where("day = ? AND to_timestamp(?, ?)::time without time zone BETWEEN open AND close", weekDay, curTime, formatStr).limit(3).order("RANDOM()")
+    		end
+    		#if user is willing to increase price first, drop price constraint
+    		if distance == 0
+    			@suggestions = RestaurantVital.where("restaurant_vital.restaurant_type ilike any ( array[?] ) AND ( distance ( latitude::REAL, longitude::REAL, ?::REAL, ?::REAL ) / 1609.344 ) <= 50", prefsStr, lat, long).joins("JOIN restaurant_theme ON restaurant_vital.id = restaurant_theme.id JOIN restaurant_times ON restaurant_times.id = restaurant_vital.id").where("rating::REAL >= 3.0").where("day = ? AND to_timestamp(?, ?)::time without time zone BETWEEN open AND close", weekDay, curTime, formatStr).limit(3).order("RANDOM()")
+    		end
+    	end
+
+    	@suggestions.each do |suggestion|
+    		types = suggestion["restaurant_type"]
+    		#if type is recommended and not pref'd, then add necessary elements
+    		if (prefs.any? {|word| types.include? word}) == false and (recd.any? {|word| types.include? word}) == true
+    			print "FOUND A REC!\n\n"
+    		end
+    	end
 
     	#query using homelat/homelong - preload data for toggle
-    	@home_suggestions = RestaurantVital.where("restaurant_type ilike any ( array[?] ) AND location like ?", prefsStr, homeCityState).where(" ( distance ( latitude::REAL, longitude::REAL, ?::REAL, ?::REAL ) / 1609.344 ) <= 25", homelat, homelong).limit(3).order("RANDOM()")
-    	print @home_suggestions[1]["name"] + "\n\n"
+    	#@home_suggestions = RestaurantVital.where("restaurant_type ilike any ( array[?] ) AND location like ?", prefsStr, homeCityState).where(" ( distance ( latitude::REAL, longitude::REAL, ?::REAL, ?::REAL ) / 1609.344 ) <= 25", homelat, homelong).limit(3).order("RANDOM()")
+    	#print @home_suggestions[1]["name"] + "\n\n"
+
+    	#go through recs and tag the ones that are suggested for the user - not resulting directly from their prefs
   	end
 end
